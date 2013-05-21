7cd181b4ec5a97e0cbcde245f0381156e3295ff1
Zarrar Shehzad <czarrar@gmail.com>
2013-05-21 12:35:34 -0400

    install my packages from github

--- connectir_install.R
+++ connectir_install.R
@@ -1,4 +1,4 @@
-#!/usr/bin/env Rscript --vanilla
+#!/usr/bin/env/Rscript
 
 basedir <- getwd()
 run <- function(cmd) {
@@ -16,11 +16,11 @@ cat("Installing various dependencies\n")
 install.packages(
     c("foreach", "doMC", "multicore", "getopt", "optparse", 
         "bigmemory", "biganalytics")
-)
+, repos="http://cran.us.r-project.org")
 install.packages(
     c("plyr", "Rcpp", "RcppArmadillo", "inline"), 
     dependencies=TRUE
-)
+, repos="http://cran.us.r-project.org")
 
 # Install BigAlgebra
 cat("\nInstalling bigalgebra\n")
@@ -28,20 +28,25 @@ run("svn checkout svn://scm.r-forge.r-project.org/svnroot/bigmemory")
 setwd("bigmemory/pkg")
 run("R CMD INSTALL bigalgebra")
 setwd(basedir)
-run("rm -r bigmemory")
+run("rm -rf bigmemory")
 
 # Installing niftir and friends
 cat("\nInstalling connectir and friends\n")
-run("svn checkout svn://scm.r-forge.r-project.org/svnroot/niftir")
-setwd("niftir/pkg")
+run("git clonse https://github.com/czarrar/bigextensions.git")
+setwd("bigextensions")
 run("R CMD INSTALL bigextensions")
+run("git clone https://github.com/czarrar/niftir.git")
+setwd("niftir")
 run("R CMD INSTALL niftir")
+run("git clone https://github.com/czarrar/connectir.git")
+setwd("connectir")
 run("R CMD INSTALL connectir")
 cat("\nCopying scripts for connectir\n")
 bindir <- dirname(system("which R", intern=TRUE))
 system(sprintf("cp connectir/inst/scripts/*.R %s/", bindir))
+system(sprintf("rm -f %s/*_worker.R", bindir))
 system(sprintf("chmod +x %s/*.R", bindir))
 setwd(basedir)
-run("rm -r niftir")
+run("rm -rf bigextensions niftir connectir")
 
 # TODO: add copying of scripts to some bin location
